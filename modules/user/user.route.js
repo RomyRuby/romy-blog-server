989a3bbdc14fa01d81aff00f80653452ed36e219
张怡琳 <zhangyilin@shgbit.com>
2024-07-10 00:28:41 +0800

    请求路由的中间处理

--- modules/user/user.route.js
+++ modules/user/user.route.js
@@ -1,6 +1,7 @@
 const userController = require('./user.controller');
+const { checkRoles } = require('../../tools/acl');
 
 
 module.exports = router => {
-  router.get('/users/info', userController.getInfo);
+  router.get('/users/info', checkRoles('test'), userController.getInfo);
 };
---
+++ tools/acl.js
@@ -0,0 +1,9 @@
+
+
+exports.checkRoles = roles => {
+  return async function (ctx, next) {
+    ctx.state.roles = roles;
+    next();
+  };
+};
+
