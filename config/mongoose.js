1756713cdf1615b9bef57d26ff76d848f046b50b
Romy <846261434@qq.com>
2024-07-30 13:28:31 +0800

    fix：mongo

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,13 +3,13 @@ const path = require('path');
 const glob = require('glob');
 
 exports.connect = async () => {
-  const uri = 'mongodb://127.0.0.1:27017/server';
+  const uri = 'mongodb://admin:password/127.0.0.1:27017server/?authSource=admin';
   await mongoose.connect(uri, { useNewUrlParser: true }).catch(err => console.log(err));
-  console.log('mongo connected : ' + uri);
+  console.log('mongo connected success: ' + uri);
 };
 
 exports.loadModels = () => {
-  const filePattern = path.resolve('./modules/*/*.model.js');//TODO: 这里的路径是怎么算的？
+  const filePattern = path.resolve('./modules/*/*.model.js');
   const files = glob.sync(filePattern);
   files.forEach(file => require(file));
 };
